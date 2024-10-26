e49fae28b40f66a7049b8bc063d472e527194bb3
conqueror~ <121376914+conqueror231@users.noreply.github.com>
2024-10-26 14:20:31 +0300

    v 1.2.7.7
    added logic for king movement

--- Code/Classes/ChessPieces/King.cpp
+++ Code/Classes/ChessPieces/King.cpp
@@ -1,11 +1,34 @@
 #include "King.h"
-
+#include <iostream>
+#include "../ChessBoard.h"
 bool King::Move(sf::Vector2i newPosition)
 {
-	return false;
+
+    int distanceX = std::abs(newPosition.x - this->position.x);
+    int distanceY = std::abs(newPosition.y - this->position.y);
+
+    if ((distanceX == 1 && distanceY == 0) || (distanceX == 0 && distanceY == 1) || (distanceX == 1 && distanceY == 1))
+    {
+        this->position = newPosition;
+        return true;
+    }
+
+	std::cout << "Invalid move" << std::endl;
+    return false;
 }
 
 bool King::Attack(ChessPiece& targetPiece)
 {
-	return false;
+	if (this->isWhite == targetPiece.isWhite)
+	{
+		std::cout << "Ally piece" << std::endl;
+		return false;
+
+	}
+	if (Move(targetPiece.GetPosition()))
+		ChessBoard::getInstance().removeChessPiece(targetPiece);
+
+
+	return true;
+
 }
